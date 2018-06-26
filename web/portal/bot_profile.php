04f410fcb6ed47861af73bbf498275d021d25d09
kudakwashe siziva <9620622+kaysiz@users.noreply.github.com>
2018-06-26 03:50:38 +0200

    write json file

--- web/portal/bot_profile.php
+++ web/portal/bot_profile.php
@@ -43,6 +43,24 @@
                                     </div>
                                 </div>
 
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label>Bot Image</label>
+                                            <input type="text" name="help" class="form-control" placeholder="Help for using the bot" required>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label>Bot Image</label>
+                                            <input type="text" name="about" class="form-control" placeholder="About Bot" required>
+                                        </div>
+                                    </div>
+                                </div>
+
                                 <button type="submit" class="btn btn-info btn-fill pull-right">Update Profile</button>
                                 <div class="clearfix"></div>
                             </form>
