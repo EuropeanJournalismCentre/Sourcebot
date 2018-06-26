33d4afdfff93a2af0fa64a1c373b53a6d767efd9
kudakwashe siziva <9620622+kaysiz@users.noreply.github.com>
2018-06-26 14:54:11 +0200

    added files

--- web/portal/bot_profile.php
+++ web/portal/bot_profile.php
@@ -10,7 +10,6 @@
         $string = file_get_contents('./bot_details.json');
         $bot_details = json_decode($string, true);
         var_dump($bot_details);
-        die();
     }
     include "./includes/header.php";
  ?>
