0db6c069b195794697d2cc10f055fcb9b5f73708
kudakwashe siziva <9620622+kaysiz@users.noreply.github.com>
2018-06-26 02:41:28 +0200

    write json file

--- web/portal/bot_profile.php
+++ web/portal/bot_profile.php
@@ -3,11 +3,11 @@
         $target_dir = "img/bot-images/";
         $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
         $uploadOk = 1;
-        $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+        $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 // Check if image file is a actual image or fake image
-        if(isset($_POST["submit"])) {
+        if (isset($_POST["submit"])) {
             $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-            if($check !== false) {
+            if ($check !== false) {
                 echo "File is an image - " . $check["mime"] . ".";
                 $uploadOk = 1;
             } else {
@@ -16,18 +16,19 @@
             }
         }
         {
-        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-            echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-        } else {
-            echo "Sorry, there was an error uploading your file.";
+            if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+                echo "The file " . basename($_FILES["fileToUpload"]["name"]) . " has been uploaded.";
+            } else {
+                echo "Sorry, there was an error uploading your file.";
+            }
+            $response = array();
+            $posts = array();
+            $posts[] = array('bot_name' => $_POST['bot_name'], 'bot_image' => $_POST['bot_image']);
+            $response['posts'] = $posts;
+            $fp = fopen('results.json', 'w');
+            fwrite($fp, json_encode($response, JSON_PRETTY_PRINT));
+            fclose($fp);
         }
-        $response = array();
-        $posts = array();
-        $posts[] = array('bot_name'=> $_POST['bot_name'], 'bot_image'=> $_POST['bot_image']);
-        $response['posts'] = $posts;
-        $fp = fopen('results.json', 'w');
-        fwrite($fp, json_encode($response, JSON_PRETTY_PRINT));
-        fclose($fp);
     }
     include "./includes/header.php";
     ?>
