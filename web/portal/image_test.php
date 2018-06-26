45d72c145accf9f7501b36ab8ad5694dfd06272b
kudakwashe siziva <9620622+kaysiz@users.noreply.github.com>
2018-06-26 02:07:33 +0200

    write json file

--- web/portal/image_test.php
+++ web/portal/image_test.php
@@ -3,5 +3,5 @@
 $url = file_get_contents("http://source.co.zw/wp-json/wp/v2/posts/7324?_embed");
 $arr = json_decode($url,true);
 
-var_dump($url[25]);
+var_dump($url["_links"]["wp:featuredmedia"]);
 die();
