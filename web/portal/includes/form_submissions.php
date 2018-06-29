319b0c017a7c4332e7c1654cc834b3caabebfb89
kudakwashe siziva <9620622+kaysiz@users.noreply.github.com>
2018-06-29 23:21:08 +0200

    testing admin users

--- web/portal/includes/form_submissions.php
+++ web/portal/includes/form_submissions.php
@@ -3,7 +3,7 @@
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         if (isset($_POST["bot_profile"])){
             $response = array('bot_name' => $_POST['bot_name'], 'bot_image' => $_POST['bot_image'], 'help' => $_POST['help'], 'about' => $_POST['about']);
-            $fp = fopen('bot_details.json', 'w');
+            $fp = fopen('../bot_details.json', 'w');
             fwrite($fp, json_encode($response, JSON_PRETTY_PRINT));
             fclose($fp);
         }
