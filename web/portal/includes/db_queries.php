4fb4872ce7dcab0f104e4854482139c803b5e73d
kudakwashe siziva <9620622+kaysiz@users.noreply.github.com>
2018-06-30 22:55:16 +0200

    users table

--- web/portal/includes/db_queries.php
+++ web/portal/includes/db_queries.php
@@ -33,11 +33,8 @@ function retrieve_messenger_users($db){
 function retrieve_messenger_user($facebook_id, $db){
 	//Check to see if the user is in the Database. If so retrieve No. of failed allempts. If not add them to it the db and retrive No. of failed attempts 
 	$query = "SELECT * FROM messenger_users WHERE facebook_id = '" . $facebook_id . "'";
-	// $result = pg_query($db, $query);
-
-	if (pg_num_rows($result) > 0){
-		$name = pg_fetch_result($result, 0, 0);
-	}
+	$result = pg_query($db, $query);
+	$name = pg_fetch_result($result, 0, 0);
 	return $name;
 }
 
