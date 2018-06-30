636e7f3e474e2bf7d5486566226003b045c0bb91
kudakwashe siziva <9620622+kaysiz@users.noreply.github.com>
2018-06-30 15:30:37 +0200

    testing admin users

--- web/portal/includes/form_submissions.php
+++ web/portal/includes/form_submissions.php
@@ -16,7 +16,7 @@
             $permissions = $_POST['role'];
             $password = hash('sha256', $_POST['password']);
             $time= date('Y-m-d H:i:s', time());
-            var_dump(create_admin_user($name, $email, $password, $permissions, $time, $time, $db));
+            echo error_log(create_admin_user($name, $email, $password, $permissions, $time, $time, $db));
             // header('Location: ' . $_SERVER["HTTP_REFERER"] );
             // exit;
             die();
