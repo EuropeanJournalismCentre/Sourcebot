74a35ec137d8534bd561ea5082e6512b35facd2e
kudakwashe siziva <9620622+kaysiz@users.noreply.github.com>
2018-06-30 18:02:16 +0200

    modified queries

--- web/portal/dashboard.php
+++ web/portal/dashboard.php
@@ -44,34 +44,134 @@
 
 
             <div class="row">
-                <div class="col-md-6">
+            <div class="col-md-6">
                     <div class="card ">
                         <div class="header">
-                            <h4 class="title">Lifetime Stats</h4>
-                            <p class="category">User Stats</p>
+                            <h4 class="title">Frequent search terms - Weekly</h4>
                         </div>
                         <div class="content">
-                            <div id="chartActivity" class="ct-chart"></div>
+                            <div class="table-full-width">
+                                <table class="table">
+                                    <tbody>
+                                        <tr>
+                                            <td>
+                                                <div class="checkbox">
+                                                    <input id="checkbox1" type="checkbox">
+                                                    <label for="checkbox1"></label>
+                                                </div>
+                                            </td>
+                                            <td>Send Broadcast Messages?</td>
+                                            <td class="td-actions text-right">
+                                                <button type="button" rel="tooltip" title="Edit Task" class="btn btn-info btn-simple btn-xs">
+                                                <i class="fa fa-edit"></i>
+                                            </button>
+                                                <button type="button" rel="tooltip" title="Remove" class="btn btn-danger btn-simple btn-xs">
+                                                <i class="fa fa-times"></i>
+                                            </button>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <div class="checkbox">
+                                                    <input id="checkbox2" type="checkbox" checked>
+                                                    <label for="checkbox2"></label>
+                                                </div>
+                                            </td>
+                                            <td>Download Weekly Stats?</td>
+                                            <td class="td-actions text-right">
+                                                <button type="button" rel="tooltip" title="Edit Task" class="btn btn-info btn-simple btn-xs">
+                                                <i class="fa fa-edit"></i>
+                                            </button>
+                                                <button type="button" rel="tooltip" title="Remove" class="btn btn-danger btn-simple btn-xs">
+                                                <i class="fa fa-times"></i>
+                                            </button>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <div class="checkbox">
+                                                    <input id="checkbox3" type="checkbox">
+                                                    <label for="checkbox3"></label>
+                                                </div>
+                                            </td>
+                                            <td>Sendly Weekly News Updates?</td>
+                                            <td class="td-actions text-right">
+                                                <button type="button" rel="tooltip" title="Edit Task" class="btn btn-info btn-simple btn-xs">
+                                                <i class="fa fa-edit"></i>
+                                            </button>
+                                                <button type="button" rel="tooltip" title="Remove" class="btn btn-danger btn-simple btn-xs">
+                                                <i class="fa fa-times"></i>
+                                            </button>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <div class="checkbox">
+                                                    <input id="checkbox4" type="checkbox" checked>
+                                                    <label for="checkbox4"></label>
+                                                </div>
+                                            </td>
+                                            <td>Save User Statistics?</td>
+                                            <td class="td-actions text-right">
+                                                <button type="button" rel="tooltip" title="Edit Task" class="btn btn-info btn-simple btn-xs">
+                                                <i class="fa fa-edit"></i>
+                                            </button>
+                                                <button type="button" rel="tooltip" title="Remove" class="btn btn-danger btn-simple btn-xs">
+                                                <i class="fa fa-times"></i>
+                                            </button>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <div class="checkbox">
+                                                    <input id="checkbox5" type="checkbox">
+                                                    <label for="checkbox5"></label>
+                                                </div>
+                                            </td>
+                                            <td>Save User Info?</td>
+                                            <td class="td-actions text-right">
+                                                <button type="button" rel="tooltip" title="Edit Task" class="btn btn-info btn-simple btn-xs">
+                                                <i class="fa fa-edit"></i>
+                                            </button>
+                                                <button type="button" rel="tooltip" title="Remove" class="btn btn-danger btn-simple btn-xs">
+                                                <i class="fa fa-times"></i>
+                                            </button>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <div class="checkbox">
+                                                    <input id="checkbox6" type="checkbox" checked>
+                                                    <label for="checkbox6"></label>
+                                                </div>
+                                            </td>
+                                            <td>Create Weekly Backups?</td>
+                                            <td class="td-actions text-right">
+                                                <button type="button" rel="tooltip" title="Edit Task" class="btn btn-info btn-simple btn-xs">
+                                                <i class="fa fa-edit"></i>
+                                            </button>
+                                                <button type="button" rel="tooltip" title="Remove" class="btn btn-danger btn-simple btn-xs">
+                                                <i class="fa fa-times"></i>
+                                            </button>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
 
                             <div class="footer">
-                                <div class="legend">
-                                    <i class="fa fa-circle text-info"></i> Active
-                                    <i class="fa fa-circle text-danger"></i> Dormant
-                                </div>
                                 <hr>
                                 <div class="stats">
-                                    <i class="fa fa-check"></i> Data information certified
+                                    <i class="fa fa-history"></i> Updated 3 minutes ago
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-
                 <div class="col-md-6">
                     <div class="card ">
                         <div class="header">
-                            <h4 class="title">Options</h4>
-                            <p class="category">Select Options</p>
+                            <h4 class="title">Frequent search terms - Monthly</h4>
                         </div>
                         <div class="content">
                             <div class="table-full-width">
