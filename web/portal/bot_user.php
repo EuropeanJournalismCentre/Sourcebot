e4e5fa94660be42bc133524497bf36146b03c3e9
kudakwashe siziva <9620622+kaysiz@users.noreply.github.com>
2018-06-30 23:29:17 +0200

    modified files

--- web/portal/bot_user.php
+++ web/portal/bot_user.php
@@ -17,7 +17,7 @@
                                 <a href="#">
                                     <img class="avatar border-gray" src="https://avatars3.githubusercontent.com/u/9620622?s=400&u=2ba1016f55802a2afff5fad09a7b4240ef2eb86a&v=4" alt="..." />
 
-                                    <h4 class="title">Kudakwashe Siziva<br />
+                                    <h4 class="title"><?= $user->name;?><br />
                                         <small> Gender : Male</small><br />
                                         <small> Email  : this@that.com</small>
                                     </h4>
