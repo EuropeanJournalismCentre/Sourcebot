9c9c324391dc7bd522a5a00d7b808d251a281e12
kudakwashe siziva <9620622+kaysiz@users.noreply.github.com>
2018-06-30 14:59:51 +0200

    testing admin users

--- web/portal/includes/db_queries.php
+++ web/portal/includes/db_queries.php
@@ -32,23 +32,18 @@ function create_messenger_error_log($message, $timestamp, $description, $type, $
 	$result = pg_query($db, $query);
 }
 
-function create_admin_user($name, $email, $password, $permissions, $last_login, $sign_up_timestamp){
+function create_admin_user($name, $email, $password, $permissions, $last_login, $sign_up_timestamp,$db){
 	//Check to see if the user is in the Database. If not add them to the db. 
-	$query = "SELECT name FROM admin_users WHERE email= '" . $email . "'";
-	$result = pg_query($db, $query);
+	// $query = "SELECT name FROM admin_users WHERE email= '" . $email . "'";
+	// $result = pg_query($db, $query);
 
-	if (pg_num_rows($result) > 0){
-		$data = ['message'=>'User already exists'];
-	}else{
+	// if (pg_num_rows($result) > 0){
+	// 	$data = ['message'=>'User already exists'];
+	// }else{
 		$query = "INSERT INTO admin_users (id, name, email, password, permissions, last_login, sign_up_timestamp) 
             VALUES (DEFAULT, '" . $name . "','" . $email . "','" . $password . "','" . $permissions . "','" . $time . "', '" . $time . "')";
 		$result = pg_query($db, $query);
-		if ($result){
-			$data = ['message' => 'User added successfully'];
-		}
-	}
-	header('Content-type: application/json');
-	echo json_encode( $data );
+	// }
 }
 
 // function update_admin_user($name, $email){
