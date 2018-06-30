3dfa2fd4fba587c28e5ada49dc8182368246364e
kudakwashe siziva <9620622+kaysiz@users.noreply.github.com>
2018-06-30 17:57:46 +0200

    modified queries

--- web/portal/dashboard.php
+++ web/portal/dashboard.php
@@ -2,35 +2,11 @@
     <div class="content">
         <div class="container-fluid">
             <div class="row">
-                <div class="col-md-4">
-                    <div class="card">
 
-                        <div class="header">
-                            <h4 class="title">User's Statistics</h4>
-                            <p class="category">Last Week's Performance</p>
-                        </div>
-                        <div class="content">
-                            <div id="chartPreferences" class="ct-chart ct-perfect-fourth"></div>
-
-                            <div class="footer">
-                                <div class="legend">
-                                    <i class="fa fa-circle text-info"></i> Active
-                                    <i class="fa fa-circle text-danger"></i> Inactive Over 2 Weeks
-                                    <i class="fa fa-circle text-warning"></i> Inactive
-                                </div>
-                                <hr>
-                                <div class="stats">
-                                    <i class="fa fa-clock-o"></i> Last 7 days
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-md-8">
+                <div class="col-md-6">
                     <div class="card">
                         <div class="header">
-                            <h4 class="title">Users Behavior</h4>
+                            <h4 class="title">User Statics - Weekly</h4>
                             <p class="category">24 Hours performance</p>
                         </div>
                         <div class="content">
@@ -41,9 +17,23 @@
                                     <i class="fa fa-circle text-danger"></i> Inactive Over A Week
                                     <i class="fa fa-circle text-warning"></i> Inactive
                                 </div>
-                                <hr>
-                                <div class="stats">
-                                    <i class="fa fa-history"></i> Updated 3 minutes ago
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="card">
+                        <div class="header">
+                            <h4 class="title">User Statics - Monthly</h4>
+                            <p class="category">24 Hours performance</p>
+                        </div>
+                        <div class="content">
+                            <div id="chartHours" class="ct-chart"></div>
+                            <div class="footer">
+                                <div class="legend">
+                                    <i class="fa fa-circle text-info"></i> Active
+                                    <i class="fa fa-circle text-danger"></i> Inactive Over A Week
+                                    <i class="fa fa-circle text-warning"></i> Inactive
                                 </div>
                             </div>
                         </div>
