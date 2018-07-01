cb49c577f0ec09243c107046b18fabc723fd945d
kudakwashe siziva <9620622+kaysiz@users.noreply.github.com>
2018-07-01 02:55:27 +0200

    modified files

--- web/portal/bot_user.php
+++ web/portal/bot_user.php
@@ -3,7 +3,7 @@
     $facebook_id = $_GET['id'];
     $user = retrieve_messenger_user($facebook_id, $db);
     $messages = retrieve_messenger_messages($db);
-    var_dump($messages);
+    // var_dump($messages);
 ?>
     <div class="content">
         <div class="container-fluid">
