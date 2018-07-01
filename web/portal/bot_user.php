36ddce5c672d4e852de0a4a554a32490b9f797c7
kudakwashe siziva <9620622+kaysiz@users.noreply.github.com>
2018-07-01 02:49:37 +0200

    modified files

--- web/portal/bot_user.php
+++ web/portal/bot_user.php
@@ -32,21 +32,21 @@
                         <div class="header">
                             <h4 class="title">Messages</h4>
                         </div>
-                        <div class="content" style="overflow:scroll;">
+                        <div class="content">
                             <div id="accordion">
                                 <?php foreach($messages as $key=>$value){ ?>
                                     <div class="card">
                                     <div class="card-header" id="headingTwo">
                                         <h5 class="mb-0">
-                                            <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                            <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#<?= $value['id'];?>" aria-expanded="false" aria-controls="collapseTwo">
                                                 <?= $value['log_timestamp'];?>
                                             </button>
                                         </h5>
                                     </div>
-                                    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+                                    <div id="<?= $value['id'];?>" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
                                         <div class="card-body">
-                                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-                                        </div>
+                                        <?= $value['log_timestamp'];?>                                        
+                                    </div>
                                     </div>
                                 </div>
                                 <?php } ?>
