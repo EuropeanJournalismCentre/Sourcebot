31e1dca37d979f81eb569af53d2f7b809c96b986
kudakwashe siziva <9620622+kaysiz@users.noreply.github.com>
2018-07-02 11:23:28 +0200

    chats

--- web/portal/dashboard.php
+++ web/portal/dashboard.php
@@ -126,41 +126,118 @@
     </div>
 <?php include "./includes/footer.php"; ?>
 <script>
-var ctx = document.getElementById("myChart");
-var myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
-            }]
-        }
-    }
-});
+    var lineChartData = {
+	labels : ["USA","UK","UAE","AUS","IN","SA"],
+	datasets : [
+		{
+			label: "My dataset",
+			fillColor : "rgba(255,255,255,0)",
+			strokeColor : "#fff",
+			pointColor : "#00796b ",
+			pointStrokeColor : "#fff",
+			pointHighlightFill : "#fff",
+			pointHighlightStroke : "rgba(220,220,220,1)",
+			data: [65, 45, 50, 30, 63, 45]
+		}
+	]
+
+}
+
+var polarData = [
+		{
+			value: 4800,
+			color:"#f44336",
+			highlight: "#FF5A5E",
+			label: "USA"
+		},
+		{
+			value: 6000,
+			color: "#9c27b0",
+			highlight: "#ce93d8",
+			label: "UK"
+		},
+		{
+			value: 1800,
+			color: "#3f51b5",
+			highlight: "#7986cb",
+			label: "Canada"
+		},
+		{
+			value: 4000,
+			color: "#2196f3 ",
+			highlight: "#90caf9",
+			label: "Austrelia"
+		},
+		{
+			value: 5500,
+			color: "#ff9800",
+			highlight: "#ffb74d",
+			label: "India"
+		},
+		{
+			value: 2100,
+			color: "#009688",
+			highlight: "#80cbc4",
+			label: "Brazil"
+		},
+		{
+			value: 5000,
+			color: "#00acc1",
+			highlight: "#4dd0e1",
+			label: "China"
+		},
+		{
+			value: 3500,
+			color: "#4caf50",
+			highlight: "#81c784",
+			label: "Germany"
+		}
+
+
+
+	];	
+		
+
+
+
+window.onload = function(){
+	var trendingLineChart = document.getElementById("myChart").getContext("2d");
+	window.trendingLineChart = new Chart(trendingLineChart).Line(data, {		
+		scaleShowGridLines : true,///Boolean - Whether grid lines are shown across the chart		
+		scaleGridLineColor : "rgba(255,255,255,0.4)",//String - Colour of the grid lines		
+		scaleGridLineWidth : 1,//Number - Width of the grid lines		
+		scaleShowHorizontalLines: true,//Boolean - Whether to show horizontal lines (except X axis)		
+		scaleShowVerticalLines: false,//Boolean - Whether to show vertical lines (except Y axis)		
+		bezierCurve : true,//Boolean - Whether the line is curved between points		
+		bezierCurveTension : 0.4,//Number - Tension of the bezier curve between points		
+		pointDot : true,//Boolean - Whether to show a dot for each point		
+		pointDotRadius : 5,//Number - Radius of each point dot in pixels		
+		pointDotStrokeWidth : 2,//Number - Pixel width of point dot stroke		
+		pointHitDetectionRadius : 20,//Number - amount extra to add to the radius to cater for hit detection outside the drawn point		
+		datasetStroke : true,//Boolean - Whether to show a stroke for datasets		
+		datasetStrokeWidth : 3,//Number - Pixel width of dataset stroke		
+		datasetFill : true,//Boolean - Whether to fill the dataset with a colour				
+		animationSteps: 15,// Number - Number of animation steps		
+		animationEasing: "easeOutQuart",// String - Animation easing effect			
+		tooltipTitleFontFamily: "'Roboto','Helvetica Neue', 'Helvetica', 'Arial', sans-serif",// String - Tooltip title font declaration for the scale label		
+		scaleFontSize: 12,// Number - Scale label font size in pixels		
+		scaleFontStyle: "normal",// String - Scale label font weight style		
+		scaleFontColor: "#fff",// String - Scale label font colour
+		tooltipEvents: ["mousemove", "touchstart", "touchmove"],// Array - Array of string names to attach tooltip events		
+		tooltipFillColor: "rgba(255,255,255,0.8)",// String - Tooltip background colour		
+		tooltipTitleFontFamily: "'Roboto','Helvetica Neue', 'Helvetica', 'Arial', sans-serif",// String - Tooltip title font declaration for the scale label		
+		tooltipFontSize: 12,// Number - Tooltip label font size in pixels
+		tooltipFontColor: "#000",// String - Tooltip label font colour		
+		tooltipTitleFontFamily: "'Roboto','Helvetica Neue', 'Helvetica', 'Arial', sans-serif",// String - Tooltip title font declaration for the scale label		
+		tooltipTitleFontSize: 14,// Number - Tooltip title font size in pixels		
+		tooltipTitleFontStyle: "bold",// String - Tooltip title font weight style		
+		tooltipTitleFontColor: "#000",// String - Tooltip title font colour		
+		tooltipYPadding: 8,// Number - pixel width of padding around tooltip text		
+		tooltipXPadding: 16,// Number - pixel width of padding around tooltip text		
+		tooltipCaretSize: 10,// Number - Size of the caret on the tooltip		
+		tooltipCornerRadius: 6,// Number - Pixel radius of the tooltip border		
+		tooltipXOffset: 10,// Number - Pixel offset from point x to tooltip edge
+		responsive: true
+		});
+
 </script>
