03ed166ee916bfe67a2460b19010fc4bfeeca6d9
kudakwashe siziva <9620622+kaysiz@users.noreply.github.com>
2018-07-02 04:43:43 +0200

    bot profile

--- web/portal/bot_profile.php
+++ web/portal/bot_profile.php
@@ -7,59 +7,7 @@
     include "./includes/header.php";
  ?>
     <div class="content">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="card">
-                        <div class="header">
-                            <h4 class="title">Bot Details</h4>
-                        </div>
-                        <div class="content">
-                            <form method="POST" enctype="multipart/form-data" action="./includes/form_submissions.php">
-                                <div class="row">
-                                    <div class="col-md-12">
-                                        <div class="form-group">
-                                            <label>Bot Name</label>
-                                            <input type="text" name="bot_name" class="form-control" placeholder="Bot Name" required>
-                                        </div>
-                                    </div>
-                                </div>
 
-                                <div class="row">
-                                    <div class="col-md-12">
-                                        <div class="form-group">
-                                            <label>Bot Image</label>
-                                            <input type="text" name="bot_image" class="form-control" placeholder="Bot image url" required>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="row">
-                                    <div class="col-md-12">
-                                        <div class="form-group">
-                                            <label>Bot Help</label>
-                                            <input type="text" name="help" class="form-control" placeholder="Help for using the bot" required>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="row">
-                                    <div class="col-md-12">
-                                        <div class="form-group">
-                                            <label>About Bot</label>
-                                            <input type="text" name="about" class="form-control" placeholder="About Bot" required>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <button type="submit" class="btn btn-info btn-fill pull-right">Update Profile</button>
-                                <div class="clearfix"></div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
         <div class="container-fluid">
             <div class="row">
                 <div class="col-md-4">
@@ -109,7 +57,7 @@
                                         <td><?= $bot_details['about'];?></td>
                                     </tr>
                                     <tr>
-                                        <td>Feature [2]/td>
+                                        <td>Feature [2]</td>
                                         <td><?= $bot_details['about'];?></td>
                                     </tr>
                                     <tr>
@@ -124,6 +72,59 @@
                                 </table>
 
                             </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="header">
+                            <h4 class="title">Bot Details</h4>
+                        </div>
+                        <div class="content">
+                            <form method="POST" enctype="multipart/form-data" action="./includes/form_submissions.php">
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label>Bot Name</label>
+                                            <input type="text" name="bot_name" class="form-control" placeholder="Bot Name" required>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label>Bot Image</label>
+                                            <input type="text" name="bot_image" class="form-control" placeholder="Bot image url" required>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label>Bot Help</label>
+                                            <input type="text" name="help" class="form-control" placeholder="Help for using the bot" required>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label>About Bot</label>
+                                            <input type="text" name="about" class="form-control" placeholder="About Bot" required>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <button type="submit" class="btn btn-info btn-fill pull-right">Update Profile</button>
+                                <div class="clearfix"></div>
+                            </form>
                         </div>
                     </div>
                 </div>
