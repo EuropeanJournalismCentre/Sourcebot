f4771ba6bc54615cb934caab8ac7e4296b035f9d
kudakwashe siziva <9620622+kaysiz@users.noreply.github.com>
2018-07-03 23:06:42 +0200

    charts testing

--- web/portal/includes/db_queries.php
+++ web/portal/includes/db_queries.php
@@ -147,7 +147,7 @@ function update_bot_messages($name, $value, $last_update, $db) {
 * Dashboard charts queries
 */
 function weekly_messenger_users($start_date, $days, $db) {
-	$query = "SELECT * from messenger_users where to_char(sign_up_timestamp) >= '" . $start_date . "' - interval '" . $days . "' day";
+	$query = "Select * from admin_users where sign_up_timestamp <= now() - interval '4' day AND sign_up_timestamp >= now() - interval '5' day";
 	var_dump($query);
 	$result = pg_query($db, $query);
 	return pg_num_rows($result);
