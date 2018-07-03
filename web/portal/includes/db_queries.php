7a512278c3dc9988f37662719422076013f252ad
kudakwashe siziva <9620622+kaysiz@users.noreply.github.com>
2018-07-03 04:50:52 +0200

    charts testing

--- web/portal/includes/db_queries.php
+++ web/portal/includes/db_queries.php
@@ -152,6 +152,7 @@ function update_bot_messages($name, $value, $last_update, $db) {
 */
 function monthly_messenger_users($start_date, $days, $db) {
 	$query = "SELECT * from messenger_users where sign_up_timestamp >= date '" . $start_date . "' - interval '" . $days . "' day;";
+	var_dump($query);
 	$result = pg_query($db, $query);
 	return pg_num_rows($result);
 }
