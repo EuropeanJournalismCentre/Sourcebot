b32d766e187b82aec1cdaf46f888270b3a67fc01
kudakwashe siziva <9620622+kaysiz@users.noreply.github.com>
2018-07-03 23:36:49 +0200

    charts testing

--- web/portal/dashboard.php
+++ web/portal/dashboard.php
@@ -7,7 +7,7 @@
     $i = 0;
     $j = 1;
     $k = 0;
-    while ($k <= 4 && $i <= 5) {
+    while ($k <= 4 && $i <= 6) {
         // $start_date = date("Y-m-d h:i:sa",strtotime('-'.$i.' days'));
         $weekly_users[] = weekly_messenger_users($i, $j, $db);
         $start_date = date("Y-m-d",strtotime('-7 days', strtotime($start_date)));
