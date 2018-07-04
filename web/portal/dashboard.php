e7003cf12d2949a68c278d5ba86d297cd8dc2373
kudakwashe siziva <9620622+kaysiz@users.noreply.github.com>
2018-07-04 05:08:24 +0200

    charts testing

--- web/portal/dashboard.php
+++ web/portal/dashboard.php
@@ -132,7 +132,7 @@
     </div>
 <?php include "./includes/footer.php"; ?>
 <script>
-    var data = <?php $weekly_users;?>
+    var data = "<?php $weekly_users;?>";
     console.log(data);
 </script>
 <!--<script src="./js/dash_charts.js"></script>-->
