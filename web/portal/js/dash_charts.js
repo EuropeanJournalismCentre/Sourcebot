c9d0d19d4e195dedb1695875b06b9d9990fb365c
kudakwashe siziva <9620622+kaysiz@users.noreply.github.com>
2018-07-04 03:53:29 +0200

    charts testing

--- web/portal/js/dash_charts.js
+++ web/portal/js/dash_charts.js
@@ -19,7 +19,7 @@ var myChart = new Chart(ctx, {
         labels: ["Day 1", "Day 2", "Day 3", "Day 4", "Day 5", "Day 6", "Day 7"],
         datasets: [{
             label: '# of weekly Users',
-            data: data5,
+            data: service,
             borderColor: [
                 'rgba(255,99,132,1)',
                 'rgba(54, 162, 235, 1)',
