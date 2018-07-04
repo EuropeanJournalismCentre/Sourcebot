91ec707884625cfd7fa0a2cda1567a6fef5533f5
kudakwashe siziva <9620622+kaysiz@users.noreply.github.com>
2018-07-04 05:21:02 +0200

    charts testing

--- web/portal/dashboard.php
+++ web/portal/dashboard.php
@@ -133,5 +133,35 @@
 <?php include "./includes/footer.php"; ?>
 <script>
     //var weekly = "<?php echo json_encode($weekly_users);?>";
+    var ctx = document.getElementById("myChart");
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ["Day 1", "Day 2", "Day 3", "Day 4", "Day 5", "Day 6", "Day 7"],
+        datasets: [{
+            label: '# of weekly Users',
+            data: [12, 19, 3, 5, 2, 3, 7],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
 </script>
-<script src="./js/dash_charts.js"></script>
+<!--<script src="./js/dash_charts.js"></script>-->
