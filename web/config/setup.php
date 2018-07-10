574d2b9cfd2134066aad33679ab65d610de3b2f9
TheGodOfAwesome <kmuvezwa@gmail.com>
2018-07-10 03:34:27 +0200

    fixing launch bugs

--- web/config/setup.php
+++ web/config/setup.php
@@ -41,7 +41,7 @@ $payload = '{
     "persistent_menu":[
         {
         "locale":"default",
-        "composer_input_disabled": true,
+        "composer_input_disabled": false,
         "call_to_actions":[
             {
                 "title":"Latest News",
